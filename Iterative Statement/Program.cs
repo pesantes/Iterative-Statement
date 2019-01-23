9f1d12da921e556c6b6312e4afd117c44c1300b5
Fernando Pesantes <pesantes@usf.edu>
2019-01-23 06:57:44 -0500

    Version 2 with a try catch added.

--- Iterative Statement/Program.cs
+++ Iterative Statement/Program.cs
@@ -13,26 +13,39 @@ namespace Iterative_Statement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter an integer value between 1 and 100");
+            Console.WriteLine("Please enter an integer value between 1 and 100: " +
+                "");
 
             // This variable will gather input from the user
             string input = Console.ReadLine();
 
+            // Try-catch to make sure no invalid input is inputted. 
+            try
+            {
+
+            
             int value_of_input = int.Parse(input);
 
-            if((value_of_input >= 1) && (value_of_input <= 100))
-            {
-                Console.WriteLine("A For Loop will execute");
-                Console.WriteLine("You have entered: " + value_of_input.ToString());
-
-                for(int i = 0; i < value_of_input; i++)
+                if ((value_of_input >= 1) && (value_of_input <= 100))
                 {
-                    Console.WriteLine("This is the current integer in the loop: " + i.ToString());
-                }
-                Console.WriteLine("To exit please press any key");
-                Console.ReadKey(true);
+                    Console.WriteLine("A For Loop will execute");
+                    Console.WriteLine("You have entered: " + value_of_input.ToString());
 
-            }
-        }
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("This is the current integer in the loop: " + i.ToString());
+                    }
+                    Console.WriteLine("To exit please press any key");
+                    Console.ReadKey(true);
+                }
+            } // End of try
+            catch
+            {
+                Console.WriteLine("Please type a whole integer between 1 and 100 next time");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+           
+            } // End of catch
+        } // End of main
     }
 }
